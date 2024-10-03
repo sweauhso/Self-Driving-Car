810e3c66b31b3bf342bb88bf48f3101732b262c5
Joshua Geng <swegengjoshua@gmail.com>
2024-10-03 13:38:14 -0600

    Added Proper Movement of Car

--- car.js
+++ car.js
@@ -7,6 +7,9 @@ class Car{
 
         this.speed = 0;
         this.acceleration = 0.2;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
 
         this.controls = new Controls();
     }
@@ -20,18 +23,53 @@ class Car{
         if(this.controls.reverse){
             this.speed -= this.acceleration;
         }
-        this.y -= this.speed;
+
+        if(this.speed > this.maxSpeed){
+            this.speed = this.maxSpeed;
+        }
+        // Negative sign just to indicate car is going backwards
+        if(this.speed < -this.maxSpeed/2){
+            this.speed = -this.maxSpeed/2
+        }
+
+        if(this.speed > 0){
+            this.speed -= this.friction;
+        }
+        if(this.speed < 0){
+            this.speed += this.friction;
+        }
+        if(Math.abs(this.speed) < this.friction){
+            this.speed = 0;
+        }
+
+        if(this.controls.left){
+            this.angle += 0.03;
+        }
+        if(this.controls.right){
+            this.angle -= 0.03;
+        }
+
+        // Implementing movement with rotation, sin of angle scaling with speed
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
     }
 
     // Method to draw black box car
     draw(ctx){
+        // Create a rotation
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width/2, 
-            this.y - this.height/2,
+            -this.width/2, 
+            -this.height/2,
             this.width,
             this.height
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
