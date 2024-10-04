fd9031a08871500a4596449d15f93aa30ec51e75
Joshua Geng <swegengjoshua@gmail.com>
2024-10-03 19:03:51 -0600

    Debugged Polygon Issues

--- car.js
+++ car.js
@@ -13,13 +13,43 @@ class Car{
 
         this.sensor = new Sensor(this);
         this.controls = new Controls();
+        this.polygon = this.#createPolygon();
     }
 
     // Method to update direction of car
     update(roadBorders){
         this.#move();
+        this.polygon = this.#createPolygon();
         this.sensor.update(roadBorders);
     }
+
+    #createPolygon() {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height) / 2;
+        const alpha = Math.atan2(this.width, this.height);
+    
+        points.push({
+            x: this.x - Math.sin(this.angle - alpha) * rad,
+            y: this.y - Math.cos(this.angle - alpha) * rad
+        });
+    
+        points.push({
+            x: this.x - Math.sin(this.angle + alpha) * rad,
+            y: this.y - Math.cos(this.angle + alpha) * rad
+        });
+    
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        });
+    
+        points.push({
+            x: this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y: this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        });
+    
+        return points;
+    } 
 
     #move(){
         // We are using adding acceleration to the speed. And everytime this happens, we are subtracting the y position by the speed. Giving the illusion of an accelerating car.
@@ -65,21 +95,14 @@ class Car{
 
     // Method to draw black box car
     draw(ctx){
-        // Create a rotation
-        ctx.save();
-        ctx.translate(this.x, this.y);
-        ctx.rotate(-this.angle);
-
+        // Using Polygon points to fill and create car
         ctx.beginPath();
-        ctx.rect(
-            -this.width/2, 
-            -this.height/2,
-            this.width,
-            this.height
-        );
-        ctx.fill();
+        ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
+        for(let i = 1; i < this.polygon.length; i++){
+            ctx.lineTo(this.polygon[i].x, this.polygon[i].y);
+        }
 
-        ctx.restore();
+        ctx.fill();
 
         this.sensor.draw(ctx);
     }
